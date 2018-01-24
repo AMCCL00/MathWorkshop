42a0a0e45a767c7f5a9d737cfb6e3ac308cc4838
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-24 15:16:39 -0500

    Commands 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Math.pow(3,5) = " + Math.pow(3,5));
+        System.out.println(10.0/7);
 
+        System.out.println("5 * Math.pow(2,5) -4 = " + (5 * Math.pow(2,5) - 4));
+        System.out.println("10.3 % 5 =" + 10.3 % 5);
+
+        System.out.println("Math.ceil(Math.random() * 100 = " + Math.ceil(Math.random() * 100));
+        System.out.println("Math.floor(Math.random() * 100 = " + Math.floor(Math.random() * 100));
+        System.out.println("Math.round(Math.random() * 100 = " + Math.round(Math.random() * 100));
     }
 }
